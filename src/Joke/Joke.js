4c0ff85d113eb5946d6dfe4cf18e5133a161626b
Sorinel Dragomir <sorinel.dragomir@hub.vauban.ro>
2018-03-06 15:57:33 +0200

    Create Joke component and print out all 3 jokes

--- src/Joke/Joke.js
+++ src/Joke/Joke.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Joke.css';
 
 const joke = (props) => {
   return (
-    <div>
+    <div className={classes.Joke}>
       <h3>Joke type: {props.type}</h3>
       <h3>Joke title: {props.title}</h3>
     </div>
